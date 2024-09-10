08ee7a7605f7ffdb5132fedecd789d2e6e898d61
tBaronDar <thedarelis@gmail.com>
2024-09-10 12:05:24 +0300

    table finished

--- components/table.tsx
+++ components/table.tsx
@@ -1,15 +1,45 @@
 "use client";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React from "react";
 
 function Table() {
 	return (
 		<Canvas>
-			<directionalLight position={[0, 0, 3]} />
-			<mesh position={[0, 1, 0]} rotation={[Math.PI / -4, 0, 0]}>
-				<boxGeometry args={[4, 6, 1]} />
-				<meshBasicMaterial color={"green"} />
-			</mesh>
+			<OrbitControls />
+			<PerspectiveCamera
+				makeDefault
+				position={[2, -10, 0]}
+				rotation={[Math.PI / 2, 0, 0]}
+			/>
+			<directionalLight position={[0, 0, 5]} />
+			<group>
+				//table
+				<mesh position={[0, 0, 0]}>
+					<boxGeometry args={[4, 6, 1]} />
+					<meshBasicMaterial color={"green"} />
+				</mesh>
+				//forward left leg
+				<mesh position={[-1.85, -2.5, -2]}>
+					<boxGeometry args={[0.15, 0.15, -3]} />
+					<meshBasicMaterial color={"brown"} />
+				</mesh>
+				//forward right leg
+				<mesh position={[1.85, -2.5, -2]}>
+					<boxGeometry args={[0.15, 0.15, -3]} />
+					<meshBasicMaterial color={"brown"} />
+				</mesh>
+				//back left leg
+				<mesh position={[-1.85, 2.5, -2]}>
+					<boxGeometry args={[0.15, 0.15, -3]} />
+					<meshBasicMaterial color={"brown"} />
+				</mesh>
+				//back right leg
+				<mesh position={[1.85, 2.5, -2]}>
+					<boxGeometry args={[0.15, 0.15, -3]} />
+					<meshBasicMaterial color={"brown"} />
+				</mesh>
+			</group>
 		</Canvas>
 	);
 }
