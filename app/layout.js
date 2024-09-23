cb0ceda5c7c38a293284f29c7f32db8e820e85d2
tBaronDar <thedarelis@gmail.com>
2024-09-23 17:36:46 +0300

    more fixes

--- app/layout.js
+++ app/layout.js
@@ -21,7 +21,15 @@ export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
 			<head>
-				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+				/>
+				<style>{`
+          * {
+            touch-action: none; /* Disable default touch actions */
+          }
+        `}</style>
 			</head>
 			<body>{children}</body>
 		</html>
